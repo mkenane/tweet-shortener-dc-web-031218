42064ac10d8c1c4e748e3615710a606892db26c7
Mor Kenane <morkenan@hotmail.com>
2018-03-01 21:54:20 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,3 +44,11 @@ def selective_tweet_shortener(tweet)
     tweet 
   end 
 end 
+
+def tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140  
+    truncated = word_substituter(tweet).chars[0..137].push("...").join("")
+  else 
+    word_substituter(tweet) 
+  end 
+end 
